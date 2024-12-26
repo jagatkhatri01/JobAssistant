61837c2f7b22b7bc3c5637b61a9a5ed2e26e67c2
jagatkhatri01 <jagatkhatri383@gmail.com>
2024-12-26 16:36:56 +0545

    feat: add signup and login pages

--- job_assistant_system/resumebuilder/urls.py
+++ job_assistant_system/resumebuilder/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
--- job_assistant_system/resumebuilder/views.py
+++ job_assistant_system/resumebuilder/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'layout.html')
 
+def signup(request):
+    return render(request , 'website/signup.html')
+
+def login(request):
+    return render(request , 'website/login.html')
