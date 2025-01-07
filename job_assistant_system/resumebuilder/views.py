ab69dba7f5f22fcd65ecad2de4cb8db26d06e49d
jagatkhatri01 <jagatkhatri383@gmail.com>
2025-01-07 15:39:28 +0545

    feat: Add resume templates feature with multiple template options

--- job_assistant_system/resumebuilder/views.py
+++ job_assistant_system/resumebuilder/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
@@ -14,27 +14,51 @@ def resume_form(request):
         resume_form = ResumeForm(request.POST)
         # print("Form data (raw):", resume_form.data)
         if resume_form.is_valid():
-            fullName = resume_form.cleaned_data.get('fullName')
-            Email = resume_form.cleaned_data.get('Email')
-            contactNo = resume_form.cleaned_data.get('contactNo')
-            address = resume_form.cleaned_data.get('address')
-            summary = resume_form.cleaned_data.get('summary')
             
-            
-            context = {
-                'full_name' : fullName,
-                'email': Email,
-                'contact_no':contactNo,
-                'address':address,
-                'summary': summary
-            }
+            resume = resume_form.save()
+            request.session['resume_id'] = resume.id
 
-            return render(request, 'resumebuilder/resume_template1.html', context)
+            return redirect('template_selector')
             
         else:
             print("form errors", resume_form.errors)
 
     else:
         resume_form = ResumeForm()
+
+    return render(request, 'resumebuilder/resume_form.html', {'resume_form':resume_form})
+
+
+def template_selector(request):
+    resume_id = request.session.get('resume_id')
+    if not resume_id:
+        return redirect('create_resume')
+
+
+    resume = get_object_or_404(Resume, id=resume_id)
+    templates = [
+        'resumebuilder/resume_template1.html',
+        'resumebuilder/resume_template2.html'
+    ]
+    context = {
+        'resume':resume,
+        'templates':templates
+    }
+    return render(request, 'template_selector.html', context)
+
+
+def edit_resume(request, resume_id):
+    resume = get_object_or_404(Resume, id=resume_id)
+    if request.method == 'POST':
+        resume_form = ResumeForm(request.POST, instance=resume)
+        if resume_form.is_valid():
+            resume = resume_form.save()
+            return redirect('template_selector')
+            
+        else:
+            print("form errors", resume_form.errors)
+
+    else:
+        resume_form = ResumeForm(instance=resume)
 
     return render(request, 'resumebuilder/resume_form.html', {'resume_form':resume_form})
