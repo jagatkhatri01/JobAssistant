59a1de83a3564b8b9dfb502e6bd53d10f5588e33
jagatkhatri01 <119671651+jagatkhatri01@users.noreply.github.com>
2025-04-17 21:21:37 +0545

    Update utils.py

--- job_assistant_system/core/utils.py
+++ job_assistant_system/core/utils.py
@@ -4,23 +4,30 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.conf import settings
 
+from django.urls import reverse
+
 def send_verification_email(user, request):
-    subject = 'Verify Your Email Address'
-    verification_url = f"http://127.0.0.1:8000/verify-email/{user.email_verification_token}/"
-
-    if settings.DEBUG:
-        verification_url = f"http://127.0.0.1:8000/verify-email/{user.email_verification_token}/"
-
-    html_message = render_to_string('registration/verification_email.html', {
+    # Generate verification URL with production domain
+    verification_url = request.build_absolute_uri(
+        reverse('verify_email', kwargs={'token': user.email_verification_token})
+    )
+    
+    # Force HTTPS in production
+    if not settings.DEBUG:
+        verification_url = verification_url.replace('http://', 'https://')
+    
+    context = {
         'user': user,
         'verification_url': verification_url,
-    })
-    plain_message = strip_tags(html_message)
+        'domain': settings.ALLOWED_HOSTS[0] if not settings.DEBUG else 'localhost:8000'
+    }
     
+    # Render and send email
+    email_html = render_to_string('registration/verification_email.html', context)
     send_mail(
-        subject=subject,
-        message=plain_message,
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[user.email],
-        html_message=html_message,
+        "Verify Your Email",
+        strip_tags(email_html),
+        settings.DEFAULT_FROM_EMAIL,
+        [user.email],
+        html_message=email_html
     )
